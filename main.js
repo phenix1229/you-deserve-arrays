5d492ba6e55572bf432e713049fa445e8e1f76e7
phenix1229 <paul.garay@codeimmersives.com>
2019-10-17 14:25:50 -0400

    completed all tasks

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-function getFirstItemFrom() {
-
+function getFirstItemFrom(ar) {
+  return ar[0];
 }
 
-function getLastItemFrom() {
-
+function getLastItemFrom(ar) {
+  return ar[ar.length - 1];
 }
 
-function getIndex3() {
-
+function getIndex3(ar) {
+  if (ar.length >= 4){
+    return ar[3];
+  } else{
+    return ar[ar.length -1];
+  }
 }
 
-function firstItemIsNumber() {
-
+function firstItemIsNumber(ar) {
+  if (typeof ar[0] == 'number'){
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function isLongList() {
-
+function isLongList(ar) {
+  if (ar.length >= 10){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
